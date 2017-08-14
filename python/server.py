17bd60737581dbeadb651174fc842c9ad48370ee
Gonzo <dggsax@gmail.com>
2017-08-14 17:21:28 -0400

    Updated server.py to interface with config json file and then create an identifiers dictionary to kick off unique implementation, but storing the dictionary is proving to be difficult

--- python/server.py
+++ python/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, session, request, make_response, json,
 from flask_socketio import SocketIO, emit, join_room, leave_room,close_room, rooms, disconnect
 import glob
 import os
+from random import randint
 from threading import Thread, Lock
 import serial
 import struct
@@ -11,6 +12,8 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+# async_mode = 'gevent'
+# async_mode = 'eventlet'
 async_mode = None
 if async_mode is None:
     try:
@@ -18,14 +21,12 @@ if async_mode is None:
         async_mode = 'eventlet'
     except ImportError:
         pass
-
     if async_mode is None:
         try:
             from gevent import monkey
             async_mode = 'gevent'
         except ImportError:
             pass
-
     if async_mode is None:
         async_mode = 'threading'
 
@@ -46,6 +47,10 @@ app.config['SECRET_KEY'] = 'secret!' #shhh don't tell anyone. Is a secret
 socketio = SocketIO(app, async_mode = async_mode)
 thread = None
 
+# Temporary main thread        
+def dataThread():
+    print("hey")
+
 # Startup has occured
 @app.route('/')
 def index():
@@ -54,17 +59,73 @@ def index():
     print ("A user connected")
     if thread is None:
         thread = Thread(target=dataThread)
+        dataThread()
         thread.daemon = True
         thread.start()
-    return render_template('pages/main.html')
+    # return render_template('/pages/main.html')
 
 # Return the configuration
 @app.route('/config', methods=['GET', 'POST'])
 def config():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/json", "config.json")
-    data = json.load(open(json_url))
-    return jsonify(data)
+    if request.method == 'GET':
+        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+        json_url = os.path.join(SITE_ROOT, "../configuration", "config.json")
+        checkJson(json_url)
+        config = json.load(open(json_url))
+        print(jsonify(config).get_data())
+        print(identifiers)
+        return jsonify(config)
+    elif request.method == 'POST':
+        print("can't really post anything yet, sorry...")
+    else:
+        print("Check your request method.")
+
+@app.route('/hello', methods=['GET'])
+def sayHello():
+    global identifiers
+    if request.method == 'GET':
+        print(identifiers)
+
+# Check and update identifiers in the json. plus other things
+def checkJson(json_url):
+    # Make global dictionary of ID's
+    global identifiers
+    identifiers = {}
+
+    # Open Json
+    with open(json_url, "r") as jsonFile:
+        config = json.load(jsonFile)
+
+    # Function to generate new unique identifier
+    def newUnique(n):
+        range_start = 10**(n-1)
+        range_end = (10**n)-1
+        return randint(range_start, range_end)
+
+    # List to store existing unique values
+    uniques = []
+    # Open up modules portion of config.json
+    modules = config[1]['modules']
+    for module in modules:
+        for instance in module:
+            for item in module[instance]:
+                # Check if module already has unique identifer
+                if 'unique' in item:
+                    # Appends existing identifier to uniques
+                    uniques.append(item['unique'])
+                else:
+                    # Generates new unique identifier
+                    unique = newUnique(3)
+                    # Checks if identifier hasn't already been used
+                    if unique not in uniques:
+                        # Assings identifier for that module
+                        item['unique'] = unique
+                identifiers[item['name']] = item['unique']
+
+    # Write modified json file
+    with open(json_url, "w") as jsonFile:
+        # Complicated dump so that everytime we modify the json it isn't minified
+        json.dump(config, jsonFile, sort_keys=True,indent=2,separators=(',',': '))
 
 # Reporting changes that occur within modules throughout the program.
 @socketio.on('reporting')
@@ -74,25 +135,5 @@ def action(content):
     # socketio.emit("{} changed to {}!".format(unique,data))
     print("{} changed to {}!".format(unique,data))
 
-if __name__ == '__main__':
+if __name__ == '__main__' or __name__ == 'server':
     socketio.run(app, port=3000, debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
