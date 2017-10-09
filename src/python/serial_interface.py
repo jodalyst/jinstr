82e0cb7ab0633f08be6b902658088cf32786ec7a
Joe Steinmeyer <jodalyst@mit.edu>
2017-10-09 07:04:11 -0400

    updating serial searcher

--- src/python/serial_interface.py
+++ src/python/serial_interface.py
@@ -1,11 +1,9 @@
 '''Automatically find USB Serial Port
 jodalyst 9/2017
 '''
-
 import serial.tools.list_ports
-import sys
-import time
-from time import sleep
+
+'''
 #Version 2.7 or Above?
 if sys.version_info[0] >2:
     version3 = True
@@ -13,26 +11,26 @@ if sys.version_info[0] >2:
 else:
     version3 = False 
     kwargs = {}
+'''
 
-def get_usb_port():
-    usb_port = list(serial.tools.list_ports.grep("USB-Serial Controller"))
-    if len(usb_port) == 1:
-        print("Automatically found USB-Serial Controller: {}".format(usb_port[0].description))
-        return usb_port[0].device
+def get_usb_ports():
+    #print(serial.tools.list_ports)
+    #usb_ports = list(serial.tools.list_ports.ListPortInfo())
+    usb_ports = list(serial.tools.list_ports.grep(""))
+    print(usb_ports)
+    if len(usb_ports) == 1:
+        print("Automatically found USB-Serial Controller: {}".format(usb_ports[0].description))
+        return usb_ports[0].device
     else:
         ports = list(serial.tools.list_ports.comports())
         port_dict = {i:[ports[i],ports[i].vid] for i in range(len(ports))}
         usb_id=None
         for p in port_dict:
             print("{}:   {} (Vendor ID: {})".format(p,port_dict[p][0],port_dict[p][1]))
-            if port_dict[p][1]==1659:
-                usb_id = p
-        if usb_id== None:
-            return False
-        else:
-            print("USB-Serial Controller: Device {}".format(p))
-            return port_dict[usb_id][0].device
-
+        return port_dict
+        
+get_usb_ports()
+'''
 s = get_usb_port()
 if s:
     ser = serial.Serial(port = s, 
@@ -61,4 +59,4 @@ if s:
 else:
     print("USB-Serial Controller Not Found")
 
-
+'''
